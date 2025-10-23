1d9a39d64979d43c1c46ac48e45d678a1fb1d914
hongdat-pham <phamhongdat314@gmail.com>
2025-10-23 13:57:44 +0700

    23/10

--- test_js/34.object/main.js
+++ test_js/34.object/main.js
@@ -1,23 +1,23 @@
-let student = {
-    fullName: "Pham Hong Dat",
-    birthYear: 2004,
-    address: {
-        city: "Nam Dinh",
-        country: "Viet Nam"
-    },
-    score: [8, 9, 10],
-    getAge: function (){
-        return 2025-this.birthYear;
-    },
-    getAge2: function (){
-        this.age = 2025-this.birthYear;
-        return this.age()
-    },
-    averageScore: function (){
-        sumScore = this.score.reduce((a,b)=>a+b,0);
-        return sumScore/3;
-    }
-};
+// let student = {
+//     fullName: "Pham Hong Dat",
+//     birthYear: 2004,
+//     address: {
+//         city: "Nam Dinh",
+//         country: "Viet Nam"
+//     },
+//     score: [8, 9, 10],
+//     getAge: function (){
+//         return 2025-this.birthYear;
+//     },
+//     getAge2: function (){
+//         this.age = 2025-this.birthYear;
+//         return this.age()
+//     },
+//     averageScore: function (){
+//         sumScore = this.score.reduce((a,b)=>a+b,0);
+//         return sumScore/3;
+//     }
+// };
 // document.writeln(student.averageScore());
 // let teacher = {
 //     "1 name": "Jacob",
@@ -80,3 +80,119 @@ let student = {
 // }
 // let entries = Object.entries(person);
 // document.writeln(entries);
+
+// Phương thức Object.assign(target, source) sử dụng để sao chép các thuộc tính từ một hoặc nhiều đối tượng nguồn (source) vào một đối tượng đích
+// const target = {};
+// const source1 = {a:1};
+// const source2 = {b:2};
+// Object.assign(target,source1,source2);
+// console.log(target);
+
+// Chúng ta có thể sử dụng toán tử spread(...) 
+// const source1 = {a:1};
+// const source2 = {b:2};
+// const target = {...source1,...source2};
+// console.log(target)
+
+// Phương thức Object.hasOwnProperty(prop) dùng để kiểm tra xem đối tượng có một thuộc tính nào đó có hay không và trả về true nếu có
+// let person = {name: "Dat", age: 21}
+// console.log(person.hasOwnProperty("name"));
+// Trả về true
+// console.log(person.hasOwnProperty("birthYear"));
+// Trả về false
+
+// Phương thức Object.freeze(object) dùng để đóng băng đối tượng, làm cho nó không thể thêm, sửa, xóa
+// let person = {name: "Dat", age:21};
+// Object.freeze(person);
+// Thêm năm sinh không thành công
+// person.birthYear = 2004;
+// Thêm email không thành công 
+// person.email = "Humg.edu.com.vn";
+// Xóa email không thành công 
+// delete person.email;
+// console.log(person);
+
+// Phương thức Object.seal(obj) ngăn không cho thêm, xóa thuộc tính chỉ cho sửa thuộc tính 
+// let person = {name:"Dat", age: 21};
+// Object.seal(person);
+// Thử xóa nhưng không được
+// delete person.name;
+// Thêm key mới cũng không được
+// person.birthYear = 2004;
+// Thay đổi value thì vẫn hợp lệ
+// person.name = "Hong Dat";
+// console.log(person)
+
+//----------------------------------------------------------------------
+
+let student = {
+    fullName: "Pham Hong Dat",
+    birthYear: 2004,
+    address: {
+        city: "Nam Dinh",
+        country: "Viet Nam"
+    },
+    score: [8, 9, 10],
+    getAge: function (){
+        return 2025-this.birthYear;
+    },
+    getAge2: function (){
+        this.age = 2025-this.birthYear;
+        return this.age()
+    },
+    averageScore: function (){
+        sumScore = this.score.reduce((a,b)=>a+b,0);
+        return sumScore/3;
+    }
+};
+
+// Hàm tạo để tạo các khuôn mẫu
+// C1: function expression  
+// const SinhVien = function(fullName, ID, birthYear, homeTown){
+//     this.fullName = fullName;
+//     this.ID = ID;
+//     this.birthYear = birthYear;
+//     this.homeTown = homeTown;
+
+//     this.showInfo = function(){
+//         return `${this.fullName} ${this.ID} ${this.birthYear} ${this.homeTown}`
+//     }
+// }
+
+// C2: function declaration
+// function SinhVien(fullName, ID, birthYear, homeTown){
+//     this.fullName = fullName;
+//     this.ID = ID;
+//     this.birthYear = birthYear;
+//     this.homeTown = homeTown;
+
+//     this.showInfo = function(){
+//         return `${this.fullName} ${this.ID} ${this.birthYear} ${this.homeTown}`
+//     }
+// }
+
+// Tạo các đối tượng cụ thể
+// const sv1 = new SinhVien("Hong Dat", 222, 2004, "Nam Dinh");
+// const sv2 = new SinhVien("Hong Thai", 223, 2003, "Ha Noi");
+
+//-----------------------------------------------------------------
+// let M = new Array();
+// console.log(typeof M)
+
+// Khai báo ES6 classes
+// C1: class expression (ít được sử dụng)
+const SinhVienEx = class{
+}
+// C2: class decoration (thường dùng vì ngắn gọn hơn)
+class SinhVien{
+    constructor(fullName, ID, birthYear, homeTown){
+        this.fullName = fullName;
+        this.ID = ID;
+        this.birthYear = birthYear;
+        this.homeTown = homeTown;
+        
+    }
+    showInfo() {
+        return `${this.fullName} ${this.ID} ${this.birthYear} ${this.homeTown}`;
+    };
+}
