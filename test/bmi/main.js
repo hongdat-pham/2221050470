9cfb25317a3aa7dad67d8bdefb1cd1212079fb51
hongdat-pham <phamhongdat314@gmail.com>
2025-10-27 16:35:34 +0700

    27/10

--- test/bmi/main.js
+++ test/bmi/main.js
@@ -1,18 +1,29 @@
 const caculateBMI = () => {
     let kg = document.getElementById("can").value;
     let m = document.getElementById("cao").value;
-    
-    let bmi = kg / (m*m);
-    document.getElementById("bmiI").value = bmi;
-    if(bmi< 18.5){
-        document.getElementsByTagName("img")[0].src="./thieu.png" 
-    } else if(bmi<24.9){
-        document.getElementsByTagName("img")[0].src = "./binhthuong.png"; 
-    } else if(bmi<29.9){
-        document.getElementsByTagName("img")[0].src = "./thua.png"; 
-    } else if(bmi<39.9){
-        document.getElementsByTagName("img")[0].src = "./beocapdo1.png"; 
-    } else{
-        document.getElementsByTagName("img")[0].src = "./beonang.png"; 
+
+    kg = parseFloat(kg);
+    m = parseFloat(m);
+
+    if (isNaN(kg) || isNaN(m) || kg <= 0 || m <= 0) {
+        alert("Vui lòng nhập đúng cân nặng và chiều cao (số dương).");
+        return;
     }
-}
+
+    let bmi = kg / (m * m);
+    document.getElementById("bmiI").value = bmi.toFixed(2);
+
+    const img = document.getElementById("anh");
+
+    if (bmi < 18.5) {
+        img.src = "./thieu.png";
+    } else if (bmi < 24.9) {
+        img.src = "./binhthuong.png";
+    } else if (bmi < 29.9) {
+        img.src = "./thua.png";
+    } else if (bmi < 39.9) {
+        img.src = "./beocapdo1.png";
+    } else {
+        img.src = "./beonang.png";
+    }
+};
