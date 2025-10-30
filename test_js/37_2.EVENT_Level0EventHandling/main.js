b5babf63ba2868458e14a21f37b2f759d00a12d9
hongdat-pham <phamhongdat314@gmail.com>
2025-10-30 20:16:16 +0700

    30/10

--- test_js/37_2.EVENT_Level0EventHandling/main.js
+++ test_js/37_2.EVENT_Level0EventHandling/main.js
@@ -2,3 +2,18 @@ const button1 = document.querySelector("#btn1");
 button1.onclick = () => {
     alert('Bạn đã click vào button')
 }
+let mess = document.querySelector("#message");
+const mouseOver = () => {
+    mess.innerHTML = "Mouse Over";
+}
+const mouseOut = () => {
+    mess.innerHTML = "Mouse Out";
+}
+const button2 = document.querySelector("#btn2");
+const setup = () => {
+    button2.onmouseover = mouseOver;
+    button2.onmouseout = mouseOut;
+}
+
+// Tất cả mọi thứ được gọi xong thì mới gọi setup
+window.onload = setup;
